2b451ef51d3c18efd1bc6458a0f94812e207e5f5
Eveline Guerrero <evelineguerrero23@gmail.com>
2022-03-20 22:43:42 -0400

    commit tree 3e38961c6349c519d70ab38468d4b6c7a2f15b3a

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters 
+
+    def full_name
+     "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles 
+        #"Khaleesi - Game of Thrones"
+        # character_names = self.characters.map(&:name)
+        #  show_names = self.shows.map(&:name)
+        self.characters.map do |c|
+        "#{c.name} - #{c.show.name}"
+        end
+    end
+
 end
